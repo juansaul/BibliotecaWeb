d558902b6591589d71936eae543b355bd0e7b8b7
juansaul <juan.saul2@live.com.mx>
2016-01-19 16:56:12 -0600

    UsuarioModel

---
+++ Biblioteca/Biblioteca/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biblioteca.Models
+{
+    public class Usuario
+    {
+        //llave primaria
+        public int PrestamoID { get; set; }
+
+        public int noControl { get; set; }
+        public string nombre { get; set; }
+        public string apellidoP { get; set; }
+        public string apellidoM { get; set; }
+        public string correo {get;set;}
+        public int cuatrimestre { get; set; }
+        public string grupo { get; set; }
+
+    }
+}
