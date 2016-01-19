94f55e7b50dffc9f7a79ebda4650616bc71fd659
juansaul <juan.saul2@live.com.mx>
2016-01-19 17:24:58 -0600

    Controlador con vistas subido

--- Biblioteca/Biblioteca/Models/Usuario.cs
+++ Biblioteca/Biblioteca/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,21 @@ namespace Biblioteca.Models
     public class Usuario
     {
         //llave primaria
+        [Key]
         public int PrestamoID { get; set; }
 
         public int noControl { get; set; }
+
         public string nombre { get; set; }
+
         public string apellidoP { get; set; }
+
         public string apellidoM { get; set; }
+
         public string correo {get;set;}
+
         public int cuatrimestre { get; set; }
+
         public string grupo { get; set; }
 
     }
