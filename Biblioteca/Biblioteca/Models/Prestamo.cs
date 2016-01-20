d64a2f4106045b7418735278f6f2872d8b416cc7
juansaul <juan.saul2@live.com.mx>
2016-01-20 10:11:24 -0600

    ModelPrestamos Hecha

---
+++ Biblioteca/Biblioteca/Models/Prestamo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Biblioteca.Models
+{
+    public class Prestamo
+    {
+        public int prestamoID { get; set; }
+
+        public string titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime fechaPrestamo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime fechaEntrega { get; set; }
+
+        // llaves foraneas
+
+        public int usuarioID { get; set; }
+        public Usuario usuario { get; set; }
+
+
+
+
+    }
+}
